9dfcbb6bcce07b2777c246195ef4577d69cc1858
rhayannematos <rhayannematos@outlook.com.br>
2025-11-01 03:53:39 -0300

    Primeiro commit: adicionando projeto Java

--- Estudante-app/src/Estudanteapp.java
+++ Estudante-app/src/Estudanteapp.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Estudanteapp {
     public static void main(String[] args) {
     int idade = 18;
@@ -5,11 +7,34 @@ public class Estudanteapp {
     char inicialDoSobrenome = 'B';
     double media = 10.0;
     boolean aprovado = true;
-    
-    System.out.println(idade);
+    String nome = "Giuliana";
+    String sobrenome = "Bezerra";
+
+    System.out.println(
+        idade + ", "
+        + inicialDoNome + ", "
+        + inicialDoSobrenome + ", "
+        + media + ", "
+        + aprovado + ", "
+        + nome + ", "
+        + sobrenome + ", "
+        );
     System.out.println(inicialDoNome);
     System.out.println(inicialDoSobrenome);
     System.out.println(media);
     System.out.println(aprovado);
+    System.out.println(nome + " " + sobrenome);
+    System.out.println(inicialDoNome + " " + inicialDoSobrenome);
+
+    System.out.println(nome.charAt(0));
+    System.out.println(nome.charAt(1));
+
+    System.out.println(nome.length());
+    System.out.println(nome.charAt(nome.length()-1));
+    System.out.println(nome.equals("Giuliana"));
+
+    Scanner scanner = new Scanner(System.in);
+    media = scanner.nextDouble();
+    System.out.println(media);
     }
 }
